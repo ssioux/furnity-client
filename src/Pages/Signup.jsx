16a7908c20c46c324bb5a1f0c4e9ed210f64865f
David Lazaro <dlazaro7@gmail.com>
2023-02-01 22:18:11 +0100

    Signup page done

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,8 +1,112 @@
 
+// CSS
+import "../css/signup.css"
+// React
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+// Services
+import { singupService } from "../services/auth.services";
+
 function Signup() {
+  const navigate = useNavigate();
+
+ // input values
+  const [usernameInput, setUsername] = useState("");
+  const [emailInput, setEmail] = useState("");
+  const [passwordInput, setPassword] = useState("");
+  const [confirmPasswordInput, setConfirmPassword] = useState("")
+// errorMessages from BE
+  const [errorMessage, setErrorMessage] = useState("");
+// Takes user info
+  const handleUsernameChange = (e) => setUsername(e.target.value);
+  const handleEmailChange = (e) => setEmail(e.target.value);
+  const handlePasswordChange = (e) => setPassword(e.target.value);
+  const handleConfirmPasswordChange = (e) => setConfirmPassword(e.target.value)
+// Send the input values to BE
+  const handleSignup = async (e) => {
+    e.preventDefault();
+
+    const newUser = {
+      name: usernameInput,
+      email: emailInput,
+      password: passwordInput,
+      confirmPassword: confirmPasswordInput,
+    };
+
+    try {
+      await singupService(newUser);
+      navigate("/login");
+    } catch (error) {
+      if (
+        (error.response && error.response.status === 406) ||
+        (error.response && error.response.status === 400)
+      ) {
+        setErrorMessage(error.response.data.message);
+      } else {
+        navigate("/error");
+      }
+    }
+  };
+
   return (
-    <div>Signup</div>
-  )
+    <section id="sign-up-container">
+      <div className="sign-up-container">
+        <form >
+          <h3>Sign-Up</h3>
+
+          <div className="input-container">
+            <input value={usernameInput} onChange={handleUsernameChange} />
+            <label className={usernameInput && "filled"} htmlFor="username">
+              User Name
+            </label>
+          </div>
+          <div className="input-container">
+            <input value={emailInput} onChange={handleEmailChange} />
+            <label className={emailInput && "filled"} htmlFor="email">
+              Email
+            </label>
+          </div>
+          <div className="input-container">
+            <input
+              value={passwordInput}
+              type="password"
+              onChange={handlePasswordChange}
+            />
+            <label className={passwordInput && "filled"} htmlFor="password">
+              Password
+            </label>
+          </div>
+
+          <div className="input-container">
+            <input
+              value={confirmPasswordInput}
+              type="password"
+              onChange={handleConfirmPasswordChange}
+            />
+            <label className={confirmPasswordInput && "filled"} htmlFor="password">
+              Confirm password
+            </label>
+          </div>
+
+         
+
+          <button
+            type="submit"
+            onClick={handleSignup}
+           className="general-btn-blue"
+          >
+            Register
+          </button>
+          {errorMessage !== "" && (
+            <p className="error-message"> * {errorMessage}</p>
+          )}
+
+        </form>
+      </div>
+
+    
+    </section>
+  );
 }
 
-export default Signup
+export default Signup;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
+// React
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+// CSS
 import "./index.css";
+// App
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+// Context
+import { AuthWrapper } from './context/auth.context';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
