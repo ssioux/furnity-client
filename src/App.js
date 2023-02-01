1e98796c7b69306ec6083ae411052703bcd6a879
David Lazaro <dlazaro7@gmail.com>
2023-02-01 13:12:28 +0100

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 
 import './App.css';
 // React
-import {Route, Routes} from "react"
+import {Route, Routes} from "react-router-dom"
 // Components
 import Navbar from './Components/Navbar';
+// Pages
 import Home from './Pages/Home';
+import Categories from './Pages/Categories';
+
 
 
 function App() {
   return (
     <div className="App">
 
-      <div>
+      <div id="logo-banner">
         <img src="../furnity-logo.png" alt="furnity-logo" />
       </div>
       <Navbar/>
+<Routes>
 
-      <Home/>
+  <Route path="/" element={<Home />} />
+  <Route path="/categories" element={<Categories/>}/>
+ 
+</Routes>
+    
     </div>
   );
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,24 @@
+// CSS
+import "../css/navbar.css"
+// React
+import {Link} from "react-router-dom"
 
 
 function Navbar() {
   return (
     <section className="navbar">
-        
+        <div className="navbar-list">
+        <ul>
+            <li><Link to="">HOME</Link></li>
+            <li><Link to="">CATEGORIES</Link></li>
+            <li><Link to="">PROFILE</Link></li>
+            <li><Link to="">LOGOUT</Link></li>
+            <li><Link to="">SIGNUP</Link></li>
+            <li><Link to="">LOGIN</Link></li>
+           
 
+        </ul>
+</div>
     </section>
   )
 }
