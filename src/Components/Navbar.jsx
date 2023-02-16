d7a29a558b923bd540702ae695b85b5cb5a78662
David Lazaro <dlazaro7@gmail.com>
2023-02-16 16:13:41 +0100

    logout working

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,18 +1,29 @@
 // CSS
 import "../css/navbar.css"
 // React
-import {Link} from "react-router-dom"
+import {Link, useNavigate} from "react-router-dom"
+// Context
+import {useContext} from "react";
+import {AuthContext} from "../context/auth.context"
 
 
 function Navbar() {
+  const navigate = useNavigate();
+  const { authenticateUser } = useContext(AuthContext);
+
+  const handleLogout = () => {
+    // Removes the token from local storage
+    localStorage.removeItem("authToken");
+    // Reload the data from user
+    authenticateUser();
+  }
   return (
     <section className="navbar animation">
 
-       
             <Link to="/">HOME</Link>
             <Link to="/categories">CATEGORIES</Link>
             <Link to="/profile">PROFILE</Link>
-            <Link to="/logout">LOGOUT</Link>
+            <Link to="/" onClick={handleLogout}>LOGOUT</Link>
             <Link to="/signup">SIGNUP</Link>
             <Link to="/login">LOGIN</Link>
 
