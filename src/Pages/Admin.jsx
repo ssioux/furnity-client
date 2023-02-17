778cd4c89e0ec75bd3358d71d002db59ffe342bf
David Lazaro <dlazaro7@gmail.com>
2023-02-17 23:08:11 +0100

    admin navbar done

--- src/Pages/Admin.jsx
+++ src/Pages/Admin.jsx
@@ -1,8 +1,17 @@
+// CSS
+import "../css/admin.css"
+// React
+import {Link} from "react-router-dom"
+
 function Admin() {
   return (
-    <section>
+    <section className="adminNav">
 
-    <h2>Admin</h2>
+            <Link to="/admin/new-category">New Category</Link>
+            <Link to="/admin/new-furniture">New Furniture</Link>
+            <Link to="/admin/orders">Orders</Link>
+            <Link to="/admin/clients">Clients</Link>
+
     </section>
 
 
