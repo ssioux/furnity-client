8791d33e47727cedd8f89fddcefc3a44623656b7
David Lazaro <dlazaro7@gmail.com>
2023-04-13 22:57:53 +0200

    category-list css wrapped

--- src/Pages/Categories.jsx
+++ src/Pages/Categories.jsx
@@ -1,49 +1,43 @@
+// css
+import "../css/category.css"
+
 // Hooks
-import { useState, useEffect } from "react"
-import { useNavigate } from "react-router-dom"
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 // Axios Services
-import { listCategoryService } from "../services/category.services"
+import { listCategoryService } from "../services/category.services";
 
 function Categories() {
+  const navigate = useNavigate();
+  const [categories, setCategories] = useState([]);
 
-const navigate = useNavigate()
-const [categories, setCategories] = useState([])
+  useEffect(() => {
+    getData();
+  }, []);
 
+  const getData = async () => {
+    try {
+      // Connexion to DB taken the category list
+      const res = await listCategoryService();
+      console.log("🚀 ~ file: Categories.jsx:24 ~ getData ~ res:", res);
 
-
-useEffect(() => {
-  
-  getData()
- 
-}, [])
-
-const getData = async () => {
-  try {
-    // Connexion to DB taken the category list
-    const res = await listCategoryService()
-    console.log("🚀 ~ file: Categories.jsx:24 ~ getData ~ res:", res)
-    
-
-    setCategories(res.data)
-
-  } catch (error) {
-    navigate('/error')
-  }
-}
+      setCategories(res.data);
+    } catch (error) {
+      navigate("/error");
+    }
+  };
   return (
-    <div>
-
+    <div className="general-container wrap">
       {categories.map((eachCategory) => {
-              return (
-               <div key={eachCategory._id}>
-                 <img src={eachCategory.image} alt="category-pic"/> 
-               </div>
-              );
-            })}
-
-
+        return (
+          <div key={eachCategory._id} className="category-box">
+            <h2>{eachCategory.name}</h2>
+            <img src={eachCategory.image} alt="category-pic" />
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
 
-export default Categories
+export default Categories;
