3688836acc69cac8f3bd0bc91cbd065c04ffa912
David Lazaro <dlazaro7@gmail.com>
2023-04-19 18:43:00 +0200

    Right NavBar Added

--- src/Components/navbar/RightNav.jsx
+++ src/Components/navbar/RightNav.jsx
@@ -1,6 +1,55 @@
-function RightNav() {
+import styled from 'styled-components';
+// React
+import {Link} from "react-router-dom"
+// Context
+import {useContext} from "react";
+import {AuthContext} from "../../context/auth.context"
+
+const Ul = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-flow: row nowrap;
+  li {
+    padding: 18px 10px;
+  }
+  @media (max-width: 768px) {
+    flex-flow: column nowrap;
+    background-color: #0D2538;
+    position: fixed;
+    transform: ${({ open }) => open ? 'translateX(0)' : 'translateX(100%)'};
+    top: 0;
+    right: 0;
+    height: 100vh;
+    width: 300px;
+    padding-top: 3.5rem;
+    transition: transform 0.3s ease-in-out;
+    li {
+      color: #fff;
+    }
+  }
+`;
+
+const RightNav = ({ open }) => {
+
+  const { authenticateUser } = useContext(AuthContext);
+
+  const handleLogout = () => {
+    // Removes the token from local storage
+    localStorage.removeItem("authToken");
+    // Reload the data from user
+    authenticateUser();
+  }
   return (
-    <div>RightNav</div>
+    <Ul open={open}>
+            <Link to="/"><li>HOME</li></Link>
+            <Link to="/categories"><li>CATEGORIES</li></Link>
+            <Link to="/profile"><li>PROFILE</li></Link>
+            <Link to="/" onClick={handleLogout}><li>LOGOUT</li></Link>
+            <Link to="/signup"><li>SIGNUP</li></Link>
+            <Link to="/login"><li>LOGIN</li></Link>
+            <Link to="/admin"><li>ADMIN</li></Link>
+
+    </Ul>
   )
 }
 
