74218528e004e612c70f0620fa939694cfa2a151
David Lazaro <dlazaro7@gmail.com>
2023-04-19 16:59:03 +0200

    create-app cleaned

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 // React
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 // CSS
 import "./index.css";
 // App
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+
 // Context
 import { AuthWrapper } from './context/auth.context';
 
@@ -21,7 +20,3 @@ root.render(
   // </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
