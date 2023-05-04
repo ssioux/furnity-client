7076179902fd69d3da8ffb2d29acb2f70747b2fc
David Lazaro <dlazaro7@gmail.com>
2023-05-04 17:31:09 +0200

    TODO eachCategory list explained

--- src/Pages/EachCategoryList.jsx
+++ src/Pages/EachCategoryList.jsx
@@ -1,9 +1,25 @@
-// TODO: Each Category furniture list.
+// React
+import { useState, useEffect } from "react";
+
 function EachCategoryList() {
-  return (
-    
-    <div>EachCategoryList</div>
-  )
+  // Furniture List from Each Category
+  const [furnituresListByCategory, setfurnituresListByCategory] = useState([]);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    // TODO: Each Category furniture list.
+    try {
+      // Conection to BE for take the furniture List of Each Category (AxiosService), adn maybe to create a new Route in the BE
+      // setfurnituresListByCategory(response)
+    } catch (error) {
+      // error message from error (400)
+      // navigate to error page (500)
+    }
+  };
+  return <div>EachCategoryList</div>;
 }
 
-export default EachCategoryList
+export default EachCategoryList;
