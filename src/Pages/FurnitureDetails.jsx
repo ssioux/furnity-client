31a46805f57114e34cf8a365060d983d19413822
David Lazaro <dlazaro7@gmail.com>
2023-05-07 15:02:07 +0200

    getting the data from BE of furniture details

--- src/Pages/FurnitureDetails.jsx
+++ src/Pages/FurnitureDetails.jsx
@@ -1,7 +1,45 @@
+// Reack Hooks
+import { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+// Axios Services
+import { detailsFurnitureService } from "../services/furniture.services";
+
 function FurnitureDetails() {
+  // Id From Params
+  const { furnitureId } = useParams();
+
+  const navigate = useNavigate();
+
+  // Each Furniture Details from API
+  const [furnitureDetails, setFurnitureDetails] = useState([]);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    try {
+      const response = await detailsFurnitureService(furnitureId);
+      console.log("first", response.data);
+      setFurnitureDetails(response.data);
+    } catch (error) {
+      navigate("/error");
+    }
+  };
+
   return (
-    <div>FurnitureDetails</div>
-  )
+    <>
+      {furnitureDetails.map((Furny) => {
+        return (
+          <div key={Furny._id}>
+            <h3>{Furny.name}</h3>
+            <p>{Furny.description}</p>
+            <img src={Furny.picture} alt="" style={{ width: 300 }} />
+          </div>
+        );
+      })}
+    </>
+  );
 }
 
-export default FurnitureDetails
+export default FurnitureDetails;
