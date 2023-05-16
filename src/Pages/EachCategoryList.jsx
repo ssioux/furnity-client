2e56a3c2c6cbf30bb71a62ce341eac0b01f3b18f
David Lazaro <dlazaro7@gmail.com>
2023-05-16 12:00:49 +0200

    cart filtered in every categoryList fixed

--- src/Pages/EachCategoryList.jsx
+++ src/Pages/EachCategoryList.jsx
@@ -29,18 +29,31 @@ function EachCategoryList() {
   const navigate = useNavigate();
   // User data
   const { isLoggedIn, user } = useContext(AuthContext);
-  const userId = user?._id
+
+  const userId = user?._id;
   
+
+
+  
+  // Function that filter the items from the user cart with the category list, to see what items has the user
+  const hasFurny = (eachFurnyId) => {
+    const searchingFurnyInCart = user?.cart.filter((eachItem) => eachItem.toString() === eachFurnyId.toString())[0];
+    
+    return searchingFurnyInCart
+  };
 
   // Furniture List from Each Category
   const [furnituresListByCategory, setfurnituresListByCategory] = useState([]);
+  console.log("🚀 furnituresListByCategory:", furnituresListByCategory)
+  
+
+  // const userHasFurny = user.cart.filter((eachItem) => eachItem._id.toString() === furnituresListByCategory._id.toString())
 
   useEffect(() => {
     getData();
   }, []);
 
   const getData = async () => {
- 
     try {
       // Getting Furniture List By Category
       const response = await eachCategoryFurnitureListService(categoryId);
@@ -51,17 +64,15 @@ function EachCategoryList() {
     }
   };
 
-  // TODO: button working if is registered, and button changing if the user has the item.
+  // TODO: button working if is registered, search cart data from axios, Id in the useEffect, BE remove addToSet  and add normal push.
   const addFurnyToCart = async (furnyId) => {
-    
-     try {
+    try {
       // service adding item to the current user cart
-      await addToCartUserService(userId, {furnyId: furnyId})
-   
+      await addToCartUserService(userId, { furnyId: furnyId });
     } catch (error) {
-      navigate("/error")
+      navigate("/error");
     }
-  }
+  };
 
   return (
     <section className="general-container wrap" style={{ marginTop: "30px" }}>
@@ -73,11 +84,32 @@ function EachCategoryList() {
               <Card.Body>
                 <Card.Title>{eachFurny.name}</Card.Title>
                 <Card.Text>{eachFurny.description}</Card.Text>
-                {isLoggedIn ? ( <Button variant="primary" onClick={() => addFurnyToCart(eachFurny._id)}>
-                  <Icon.CartPlus size={30} />
-                </Button>) : ( <Button variant="primary" onClick={() => navigate("/signup")}>
-                  <Icon.CartPlus size={30} />
-                </Button>
+                {/* Button filtered by logged */}
+                {/* {isLoggedIn ? (
+                  <Button
+                    variant="primary"
+                    onClick={() => addFurnyToCart(eachFurny._id)}
+                  >
+                    <Icon.CartPlus size={30} />
+                  </Button>
+                ) : (
+                  <Button variant="primary" onClick={() => navigate("/signup")}>
+                    <Icon.CartPlus size={30} />
+                  </Button>
+                )} */}
+               
+
+                      { hasFurny(eachFurny._id) === eachFurny._id ? (
+                  <Button
+                    variant="danger"
+                    onClick={() => addFurnyToCart(eachFurny._id)}
+                  >
+                    <Icon.CartX size={30} />
+                  </Button>
+                ) : (
+                  <Button variant="primary" onClick={() => addFurnyToCart(eachFurny._id)}>
+                    <Icon.CartPlus size={30} />
+                  </Button>
                 )}
               </Card.Body>
             </Card>
